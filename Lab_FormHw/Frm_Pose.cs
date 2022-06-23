bc686aa5ba8bd38b7f14ecee8fdf6e558c193afd
暐婷 洪 <jo3wait@gmail.com>
2022-06-23 18:16:35 +0800

    0623

--- Lab_FormHw/Frm_Pose.cs
+++ Lab_FormHw/Frm_Pose.cs
@@ -30,60 +30,74 @@ namespace Lab_FormHw
         //    }            
         //}
 
-        int countBeer = 0;        
-        int priceBeer = 0;
-        int countTequila = 0;
-        int priceTequila = 0;
-        int countWhisky = 0;
-        int priceWhisky = 0;
-        int countWine = 0;
-        int priceWine = 0;
-        int totalPrice = priceBeer + priceTequila + priceWhisky + priceWine;
+        int countBeer, countTequila, countWhisky, countWine;
+        int priceBeer, priceTequila, priceWhisky, priceWine, totalPrice;
+        int pricePerBeer = 120, pricePerTequila = 180, 
+            pricePerWhisky = 350, pricePerWine = 320;
+        string showBeer, showTequila, showWhisky, showWine;
 
         private void btnProduct1_Click(object sender, EventArgs e)
         {
             //labList.Text = "";
             countBeer += 1;
-            priceBeer = 120 * countBeer;
-            //labList.Text += $"啤酒 Beer x { countBeer}, 共{ priceBeer}元\n";
-            //lstShow();
-
-            ShowTotal();
+            priceBeer = pricePerBeer * countBeer;
+            if (countBeer >= 1)
+            {
+                showBeer = $"啤酒 Beer x { countBeer}, 共{ priceBeer}元\n";
+            }
+           
+            Total();
         }
 
         private void btnProduct2_Click(object sender, EventArgs e)
         {
             //labList.Text = "";
             countTequila += 1;
-            priceTequila = 180 * countTequila;
-            //labList.Text += $"龍舌蘭 Tequila x { countTequila}, 共{ priceTequila}元\n";
-            //lstShow();
+            priceTequila = pricePerTequila * countTequila;
+            if (countTequila >= 1)
+            {
+                showTequila = $"龍舌蘭 Tequila x { countTequila}, 共{ priceTequila}元\n";
+            }
 
-            ShowTotal();
+            Total();
         }
 
         private void btnProduct3_Click(object sender, EventArgs e)
         {
-            countWhisky += 1;
-            priceWhisky = 350 * countWhisky;
+            countWhisky += 1;            
+            priceWhisky = pricePerWhisky * countWhisky;
+            if (countWhisky >= 1)
+            {
+                showWhisky = $"威士忌 Whisky x { countWhisky}, 共{ priceWhisky}元\n";
+            }
+            Total();
         }
 
         private void btnProduct4_Click(object sender, EventArgs e)
         {
             countWine += 1;
-            priceWine = 320 * countWine;
-        }
-
-        void ShowTotal()
-        {
-            labTotal.Text = $"{totalPrice:c0}";
+            priceWine = pricePerWine * countWine;
+            if (countWine >= 1)
+            {
+                showWine = $"紅酒 Wine x { countWine}, 共{ priceWine}元\n";
+            }
+            Total();
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
             labList.Text = "尚未點餐";
+            showBeer = ""; showTequila = ""; showWhisky = "";showWine = "";
+            countBeer = countTequila = countWhisky = countWine = 0;
             labTotal.Text = "NT$ 0";
         }
+
+        void Total()
+        {
+            totalPrice = priceBeer + priceTequila + priceWhisky + priceWine;
+            labTotal.Text = $"{totalPrice:c0}";
+            labList.Text = (showBeer + showTequila + showWhisky + showWine);
+        }              
 
         private void btnCash_Click(object sender, EventArgs e)
         {
