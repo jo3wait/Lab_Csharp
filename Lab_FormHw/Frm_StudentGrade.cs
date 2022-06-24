2d2d71b7b73ce5169e17e735876c37abb25bf3f8
暐婷 洪 <jo3wait@gmail.com>
2022-06-24 18:07:32 +0800

    0624

--- Lab_FormHw/Frm_StudentGrade.cs
+++ Lab_FormHw/Frm_StudentGrade.cs
@@ -16,7 +16,7 @@ namespace Lab_FormHw
         {
             InitializeComponent();
         }
-
+        //111
         // Random rdm = new Random();
         // .Next(0, 100)
         // for loop i<20
